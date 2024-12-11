4897ff897a32680dec7e23c0a36a20bab2110d12
Muhammadrahimw <muqalla21@gmail.com>
2024-12-11 19:11:47 +0500

    half basket code

--- src/pages/basket/index.jsx
+++ src/pages/basket/index.jsx
@@ -8,6 +8,7 @@ import {Link} from "react-router-dom";
 import {CiHeart} from "react-icons/ci";
 import {FaHeart} from "react-icons/fa";
 import {reduceContext} from "../../context/useReduceContext/index.jsx";
+import {AiOutlineRight} from "react-icons/ai";
 
 function BasketPage() {
 	let {CutText} = useContext(FunctionContext);
@@ -32,7 +33,7 @@ function BasketPage() {
 	let basketProducts = JSON.parse(localStorage.getItem("basket"));
 
 	let favoriteProducts = allData?.filter((item) =>
-		basketProducts.includes(item.id)
+		basketProducts?.includes(item.id)
 	);
 
 	let addFavourite = (id) => {
@@ -40,52 +41,79 @@ function BasketPage() {
 	};
 	return (
 		<section className="my-12">
-			{favoriteProducts.length > 0 ? (
-				favoriteProducts.map((value) => (
-					<Link key={value.id} to={`/product/${value.id}/technical`}>
-						<div className="w-full p-4 rounded shadow-md">
-							<div
-								style={{backgroundImage: `url(${value.preview.images})`}}
-								className="max-w-[16em] h-[12em] bgStyle bg-cover max-[480px]:max-w-full relative">
-								{state.favorites.includes(value.id) ? (
-									<FaHeart
-										className="absolute top-0 left-0 text-3xl text-red-500 cursor-pointer"
-										onClick={(e) => {
-											e.preventDefault();
-											addFavourite(value.id);
-										}}
-									/>
-								) : (
-									<CiHeart
-										className="absolute top-0 left-0 text-3xl cursor-pointer"
-										onClick={(e) => {
-											e.preventDefault();
-											addFavourite(value.id);
-										}}
-									/>
-								)}
-							</div>
-							<div className="my-4 border-t rounded-full border-gray"></div>
-							<div className="h-[5.5em] flex flex-col justify-between items-start gap-2">
-								<p className="font-light">{value.name}</p>
-								<div className="flex justify-between items-center gap-4 w-full text-[1.1em]">
-									<p className="font-light">${value.price}</p>
-									<p className="flex items-center gap-1 font-light">
-										<span className="scale-110 text-primary">
-											<FaStar />
-										</span>
-										{value.rating}
+			<div className="text-[1.1em] text-gray font-light flex items-center gap-1 pt-6 pb-10">
+				<Link to={"/"}>Home</Link> <AiOutlineRight /> Products
+				<AiOutlineRight />
+				<span className="text-blue-500">basket</span>
+			</div>
+			<div className="w-full my-8">
+				<div className="grid w-full grid-cols-7 gap-10">
+					<p className="text-xl font-semibold">Description</p>
+					<p className="text-xl font-semibold">Due Amount</p>
+					<p className="text-xl font-semibold">Due Date</p>
+					<p className="text-xl font-semibold">Actual Amount</p>
+					<p className="text-xl font-semibold">Payment Date</p>
+					<p className="text-xl font-semibold">Count</p>
+					<p className="text-xl font-semibold">Total</p>
+				</div>
+				<div className="">
+					{favoriteProducts.length > 0 ? (
+						favoriteProducts.map((value) => (
+							<div className="grid w-full grid-cols-7 gap-10 pb-5 my-6 border-b border-gray">
+								<Link key={value.id} to={`/product/${value.id}/technical`}>
+									<div
+										style={{backgroundImage: `url(${value.preview.images})`}}
+										className="max-w-[16em] h-[5em] bgStyle bg-cover max-[480px]:max-w-full relative">
+										{state.favorites.includes(value.id) ? (
+											<FaHeart
+												className="absolute top-0 left-0 text-3xl text-red-500 cursor-pointer"
+												onClick={(e) => {
+													e.preventDefault();
+													addFavourite(value.id);
+												}}
+											/>
+										) : (
+											<CiHeart
+												className="absolute top-0 left-0 text-3xl cursor-pointer"
+												onClick={(e) => {
+													e.preventDefault();
+													addFavourite(value.id);
+												}}
+											/>
+										)}
+									</div>
+								</Link>
+								<div className="flex items-center text-xl font-semibold">
+									<p className="font-light">
+										${value.originalPrice || value.price}
+									</p>
+								</div>
+								<div className="flex items-center text-xl font-semibold">
+									<p className="font-light">{value.releaseDate}</p>
+								</div>
+								<div className="flex items-center text-xl font-semibold">
+									<p className="font-light">
+										${value.discountedPrice || value.price}
+									</p>
+								</div>
+								<div className="flex items-center justify-center text-xl font-semibold">
+									<p className="font-light">-</p>
+								</div>
+								<div></div>
+								<div className="flex items-center text-xl font-semibold">
+									<p className="font-light">
+										${value.discountedPrice || value.price}
 									</p>
 								</div>
 							</div>
-						</div>
-					</Link>
-				))
-			) : (
-				<p className="text-[3em] text-gray text-center">
-					Sizda yoqitirlgan mahsulotlar yo'q
-				</p>
-			)}
+						))
+					) : (
+						<p className="text-[3em] text-gray text-center">
+							Sizda hali mahsulotlar yo'q
+						</p>
+					)}
+				</div>
+			</div>
 		</section>
 	);
 }
